020d1d47c24100d19369b934c736ba6292841e7b
Lorenz Aigner (S2010239001) <s2010239001@students.fh-hagenberg.at>
2021-04-19 11:04:43 +0200

    Changed some names

---
+++ org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionOverview.java
@@ -0,0 +1,43 @@
+// -----BEGIN DISCLAIMER-----
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 JCrypTool Team and Contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+// -----END DISCLAIMER-----
+
+package org.jcryptool.visual.signalencryption.ui;
+
+import java.util.Base64;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Composite;
+
+public class SignalEncryptionOverview extends Composite {
+
+    SignalEncryptionOverview() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+
+}

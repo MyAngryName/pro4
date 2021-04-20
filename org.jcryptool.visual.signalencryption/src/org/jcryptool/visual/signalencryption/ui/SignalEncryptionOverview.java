8a88270fada6cb0250b355c0c8911ff1a4855b6d
Lorenz Aigner (S2010239001) <s2010239001@students.fh-hagenberg.at>
2021-04-20 16:54:08 +0200

    Made a usable class for the Overview Panel

--- org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionOverview.java
+++ org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionOverview.java
@@ -1,43 +1,164 @@
-// -----BEGIN DISCLAIMER-----
-/*******************************************************************************
- * Copyright (c) 2020, 2020 JCrypTool Team and Contributors
- *
- * All rights reserved. This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
-// -----END DISCLAIMER-----
-
+/**
+ * 
+ */
 package org.jcryptool.visual.signalencryption.ui;
 
-import java.util.Base64;
-
-import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.BusyIndicator;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Path;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.graphics.Point;
 
-public class SignalEncryptionOverview extends Composite {
 
-    SignalEncryptionOverview() {
-        super();
-        // TODO Auto-generated constructor stub
+public class SignalEncryptionOverview {
+    
+    private static int ARROW_WIDTH = 10;
+    private static int ARROW_HEAD_WIDTH = 20;
+
+    /**
+     * A simple state machine based on enum and enum methods.
+     *
+     * Use {@link STATE#next(SignalEncryptionOverview)} and {@link STATE#back(SignalEncryptionOverview)} to switch states.
+     */
+    private enum STATE {
+        STEP_1 {
+            @Override
+            protected void switchState(SignalEncryptionOverview parent) {
+                System.out.println("Switching to state 1");
+            parent.stepTwoDescription.setVisible(false);
+            parent.arrowCanvas.setVisible(false);
+            parent.back.setEnabled(false);
+            parent.next.setEnabled(true);
+            }
+
+            @Override
+            STATE next(SignalEncryptionOverview parent) {
+                STEP_2.switchState(parent);
+                return STEP_2;
+            }
+
+            @Override
+            STATE back(SignalEncryptionOverview parent) {
+                return STEP_1;
+            }
+        }, STEP_2 {
+            @Override
+            protected void switchState(SignalEncryptionOverview parent) {
+                System.out.println("Switching to state 2");
+                parent.stepTwoDescription.setVisible(true);
+                parent.arrowCanvas.setVisible(true);
+                parent.next.setEnabled(false);
+                parent.back.setEnabled(true);
+            }
+
+            @Override
+            STATE next(SignalEncryptionOverview parent) {
+                return STEP_1;
+            }
+
+            @Override
+            STATE back(SignalEncryptionOverview parent) {
+                STEP_1.switchState(parent);
+                return STEP_1;
+            }
+        };
+        
+        protected abstract void switchState(SignalEncryptionOverview parent);
+        abstract STATE next(SignalEncryptionOverview parent);
+        abstract STATE back(SignalEncryptionOverview parent);
+        
+        public STATE setInitialState(SignalEncryptionOverview parent) {
+            STEP_1.switchState(parent);
+            return STEP_1;
+        }
+        
+        };
+    private STATE currentState = STATE.STEP_1;
+
+    /*
+     * Create GridData object and give it a widthHint (how large the component would like to be).
+     */
+    private GridData gridDataWidth(int horizonalAlignment, int verticalAlignment, boolean grabExcessHorizontal, boolean grabExcessVertical, int horizontalSpan, int verticalSpan, int width) {
+    }
+
+    /*
+     * Draw an arrow on the canvas (from left to right)
+     */
+    static Path drawArrow(Canvas canvas) {
+        Path resultPath = new Path(canvas.getDisplay());
+        int width = canvas.getBounds().width;
+        int height = canvas.getBounds().height;
+        float baseLength = width - 0.2f * width;
+        
+        float lowerLineY = (height / 2) - (ARROW_WIDTH / 2);
+        float upperLineY = (height / 2) + (ARROW_WIDTH / 2);
+
+        float lowerArrowHeadY = (height / 2) - (ARROW_HEAD_WIDTH / 2);
+        float upperArrowHeadY = (height / 2) + (ARROW_HEAD_WIDTH / 2);
+
+
+        resultPath.moveTo(0, lowerLineY);
+        resultPath.lineTo(0, upperLineY);
+        resultPath.lineTo(baseLength, upperLineY);
+        resultPath.lineTo(baseLength, upperArrowHeadY);
+        resultPath.lineTo(width, height / 2);
+        resultPath.lineTo(baseLength, lowerArrowHeadY);
+        resultPath.lineTo(baseLength, lowerLineY);
+        resultPath.lineTo(0, lowerLineY);
+        return resultPath;
     }
     
-    
+    public SignalEncryptionOverview() {
+        final Display display = new Display();
+        final Shell shell = new Shell(display);
+        shell.setLayout(new FillLayout());
+        
+        createChildren(shell);
+        currentState = STATE.STEP_1.setInitialState(this);
+        
+        shell.pack();
+        
+        Composite composite = new Composite(shell, SWT.NONE);
+        composite.setSize(new Point(600, 600));
+        composite.setLayout(new RowLayout(SWT.HORIZONTAL));
+        
+        Label lblNewLabel = new Label(composite, SWT.BORDER);
+        lblNewLabel.setAlignment(SWT.CENTER);
+        lblNewLabel.setLocation(new Point(30, 50));
+        lblNewLabel.setLayoutData(new RowData(68, 23));
+        lblNewLabel.setText("Other Key");
+        
+        Label lblNewLabel_1 = new Label(composite, SWT.BORDER);
+        lblNewLabel_1.setAlignment(SWT.CENTER);
+        lblNewLabel_1.setLayoutData(new RowData(59, 19));
+        lblNewLabel_1.setText("Root Key");
+        shell.open();
 
+        while (!shell.isDisposed()) {
+            if (!display.readAndDispatch())
+                display.sleep();
+        }
+        display.dispose();
+    }
+
+    
+    private void createChildren(Shell parent) {
+        SignalEncryptionOverview instance = this;
+    }
 }

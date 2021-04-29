f23e1f98029c53bbcbd448a1469f842125cd9255
S1910239007 (Ciudin Dan-Kerko) <s1910239007@fhooe.at>
2021-04-29 11:57:30 +0200

    created overview

--- org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionOverview.java
+++ org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionOverview.java
@@ -14,6 +14,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
@@ -24,166 +25,126 @@ import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.widgets.TableColumn;
 
 
     
-public class SignalEncryptionOverview extends{
-    
-    private static int ARROW_WIDTH = 10;
-    private static int ARROW_HEAD_WIDTH = 20;
+public class SignalEncryptionOverview extends Composite {
+
+	
+    private Composite parameterComposite;
+
+    private GridLayout parameterCompositeLayout;
+
+    private Group parameterGroup;
+    // private Group publicKeyGroup;
+
+    private Text label_a;
+    private Text label_b;
+    private Text label_c;
+    private Text label_d;
+    private Text label_e;
+    private Text label_f;
+    private Text text_a;
+    private Text text_b;
+    private Text text_c;
+    private Text text_d;
+    private Text text_e;
+    private Text text_f;
+	
+	public SignalEncryptionOverview (final Composite parent, final int style) {
+		super(parent,style);
+		setLayout(new GridLayout());
+		createBody();
+	}
+    private void createBody() {
+        final Composite bodyComposite = new Composite(this, SWT.NONE);
+        GridData gd_bodyComposite = new GridData(SWT.FILL, SWT.FILL, true, true);
+        gd_bodyComposite.widthHint = 626;
+        bodyComposite.setLayoutData(gd_bodyComposite);
+        GridLayout gl_bodyComposite = new GridLayout();
+        gl_bodyComposite.marginWidth = 0;
+        gl_bodyComposite.marginHeight = 0;
+        bodyComposite.setLayout(gl_bodyComposite);
+
+        createParameterBody(bodyComposite);
+        
+    }
 
     /**
-     * A simple state machine based on enum and enum methods.
-     *
-     * Use {@link STATE#next(SignalEncryptionOverview)} and {@link STATE#back(SignalEncryptionOverview)} to switch states.
+     * Creates the parameter body, including parameter and description group.
      */
-    private enum STATE {
-        STEP_1 {
-            @Override
-            protected void switchState(SignalEncryptionOverview parent) {
-                System.out.println("Switching to state 1");
-            parent.stepTwoDescription.setVisible(false);
-            }
+    private void createParameterBody(Composite parent) {
+        parameterComposite = new Composite(parent, SWT.NONE);
+        parameterComposite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        parameterCompositeLayout = new GridLayout(2, false);
+        parameterCompositeLayout.marginWidth = 0;
+        parameterCompositeLayout.marginHeight = 0;
+        parameterComposite.setLayout(parameterCompositeLayout);
+        GridData gd_parameterComposite = new GridData(SWT.FILL, SWT.FILL, true, true);
+        gd_parameterComposite.widthHint = 561;
+        parameterComposite.setLayoutData(gd_parameterComposite);
 
-            @Override
-            STATE next(SignalEncryptionOverview parent) {
-                STEP_2.switchState(parent);
-                return STEP_2;
-            }
+        createParameterGroup(parameterComposite);
+        new Label(parameterComposite, SWT.NONE);
 
-            @Override
-            STATE back(SignalEncryptionOverview parent) {
-                return STEP_1;
-            }
-        }, STEP_2 {
-            @Override
-            protected void switchState(SignalEncryptionOverview parent) {
-                System.out.println("Switching to state 2");
-                parent.stepTwoDescription.setVisible(true);
-                parent.arrowCanvas.setVisible(true);
-                parent.next.setEnabled(false);
-                parent.back.setEnabled(true);
-            }
-
-            @Override
-            STATE next(SignalEncryptionOverview parent) {
-                return STEP_1;
-            }
-
-            @Override
-            STATE back(SignalEncryptionOverview parent) {
-                STEP_1.switchState(parent);
-                return STEP_1;
-            }
-        };
-        
-        protected abstract void switchState(SignalEncryptionOverview parent);
-        abstract STATE next(SignalEncryptionOverview parent);
-        abstract STATE back(SignalEncryptionOverview parent);
-        
-        public STATE setInitialState(SignalEncryptionOverview parent) {
-            STEP_1.switchState(parent);
-            return STEP_1;
-        }
-        
-        };
-    private STATE currentState = STATE.STEP_1;
-    
-    private Label masterSecretKeyName;
-    private Label masterSecretKeyDescription;
-    private Label secondKeyName;
-    private Label secondKeyDescription;
-    private Label thirdKeyName;
-    private Label thirdKeyDescription;
-    private Label fourthKeyName;
-    private Label fourthKeyDescription;
-    private Label fifthKeyName;
-    private Label fifthKeyDescription;
-
-    /*
-     * Create GridData object and give it a widthHint (how large the component would like to be).
-     */
-    private GridData gridDataWidth(int horizonalAlignment, int verticalAlignment, boolean grabExcessHorizontal, boolean grabExcessVertical, int horizontalSpan, int verticalSpan, int width) {
-        var gd_masterSecretKeyName = new GridData(horizonalAlignment, verticalAlignment, grabExcessHorizontal, grabExcessVertical, horizontalSpan, verticalSpan);
-        gd_masterSecretKeyName.widthHint = width;
-        return gd_masterSecretKeyName;
-    }
-
-    /*
-     * Draw an arrow on the canvas (from left to right)
-     */
-    static Path drawArrow(Canvas canvas) {
-        Path resultPath = new Path(canvas.getDisplay());
-        int width = canvas.getBounds().width;
-        int height = canvas.getBounds().height;
-        float baseLength = width - 0.2f * width;
-        
-        float lowerLineY = (height / 2) - (ARROW_WIDTH / 2);
-        float upperLineY = (height / 2) + (ARROW_WIDTH / 2);
-
-        float lowerArrowHeadY = (height / 2) - (ARROW_HEAD_WIDTH / 2);
-        float upperArrowHeadY = (height / 2) + (ARROW_HEAD_WIDTH / 2);
-
-
-        resultPath.moveTo(0, lowerLineY);
-        resultPath.lineTo(0, upperLineY);
-        resultPath.lineTo(baseLength, upperLineY);
-        resultPath.lineTo(baseLength, upperArrowHeadY);
-        resultPath.lineTo(width, height / 2);
-        resultPath.lineTo(baseLength, lowerArrowHeadY);
-        resultPath.lineTo(baseLength, lowerLineY);
-        resultPath.lineTo(0, lowerLineY);
-        return resultPath;
     }
     
-    public SignalEncryptionOverview() {
-        final Display display = new Display();
-        final Shell shell = new Shell(display);
-        shell.setSize(538, 327);
-        shell.setLayout(new FillLayout());
+    private void createParameterGroup(Composite parent) {
+    	parameterGroup = new Group(parent, SWT.NONE);
+        parameterGroup.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        parameterGroup.setLayout(new GridLayout(3, false));
+        GridData gd_parameterGroup = new GridData(SWT.FILL, SWT.FILL, false, true);
         
-        createChildren(shell);
-        currentState = STATE.STEP_1.setInitialState(this);
+        label_a = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
+        label_a.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        label_a.setText("Schlüssel");
+
+        text_a = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_a.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        text_a.setText("Beschreibung");
         
-        shell.pack();
+        label_b = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
+        label_b.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        label_b.setText("Diffie Hellman Key Pair");
+
+        text_b = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_b.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        text_b.setText("Besteht aus einem öffentlichen und privaten Teil.");
         
+        label_c = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
+        label_c.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        label_c.setText("Shared Secret Key");
+
+        text_c = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_c.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        text_c.setText("Geheimer Schlüssel der von den Kommunikationspartnern mittels einem vereinbarten Schlüsselaustausch erstellt wird. Dieser Schlüssel dient als erster Schlüssel");
         
-        shell.open();
+        label_d = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
+        label_d.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        label_d.setText("Root Chain Key");
 
-        while (!shell.isDisposed()) {
-            if (!display.readAndDispatch())
-                display.sleep();
-        }
-        display.dispose();
-    }
-
-    
-    private void createChildren(Shell parent) {
-        var simpleComposite = new Composite(parent, SWT.NONE);
-        simpleComposite.setLayout(new GridLayout(2, true));
-        SignalEncryptionOverview instance = this;
-        masterSecretKeyName = new Label(simpleComposite, SWT.BORDER | SWT.CENTER);
-        masterSecretKeyName.setText("Step 1");
-        masterSecretKeyName.setLayoutData(gridDataWidth(SWT.FILL, SWT.CENTER, false, false, 1, 1, 100));
-
-        masterSecretKeyDescription = new Label(simpleComposite, SWT.BORDER | SWT.CENTER);
-        masterSecretKeyDescription.setText("Step 2");
+        text_d = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_d.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        text_d.setText("");
         
-        secondKeyName = new Label(simpleComposite, SWT.BORDER | SWT.CENTER);
-        secondKeyName.setText("Step 1");
+        label_e = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
+        label_e.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        label_e.setText("Sending Chain Key");
 
-        secondKeyDescription = new Label(simpleComposite, SWT.BORDER | SWT.CENTER);
-        secondKeyDescription.setText("Step 2");
-
-        thirdKeyName = new Label(simpleComposite, SWT.BORDER | SWT.CENTER);
-        thirdKeyName.setText("Step 1");
-
-        fourthKeyDescription = new Label(simpleComposite, SWT.BORDER | SWT.CENTER);
-        fourthKeyDescription.setText("Step 2");
+        text_e = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_e.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        text_e.setText("");
         
-        fifthKeyName = new Label(simpleComposite, SWT.BORDER | SWT.CENTER);
-        fifthKeyName.setText("Step 1");
+        label_f = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
+        label_f.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        label_f.setText("Receiving Chain Key");
 
-        new Label(simpleComposite, SWT.NONE);
+        text_f = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_f.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        text_f.setText("");
     }
 }

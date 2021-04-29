5d6afb6df9952470592f7008495a3ff9dda01e4f
S1910239007 (Ciudin Dan-Kerko) <s1910239007@fhooe.at>
2021-04-29 12:11:53 +0200

    Changed Overview

--- org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionOverview.java
+++ org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionOverview.java
@@ -56,15 +56,12 @@ public class SignalEncryptionOverview extends Composite {
     private Text text_f;
 	
 	public SignalEncryptionOverview (final Composite parent, final int style) {
-		super(parent,style);
-		setLayout(new GridLayout());
+		super(parent,SWT.NONE);
 		createBody();
 	}
     private void createBody() {
+        setLayout(new GridLayout(1, false));
         final Composite bodyComposite = new Composite(this, SWT.NONE);
-        GridData gd_bodyComposite = new GridData(SWT.FILL, SWT.FILL, true, true);
-        gd_bodyComposite.widthHint = 626;
-        bodyComposite.setLayoutData(gd_bodyComposite);
         GridLayout gl_bodyComposite = new GridLayout();
         gl_bodyComposite.marginWidth = 0;
         gl_bodyComposite.marginHeight = 0;
@@ -84,66 +81,77 @@ public class SignalEncryptionOverview extends Composite {
         parameterCompositeLayout.marginWidth = 0;
         parameterCompositeLayout.marginHeight = 0;
         parameterComposite.setLayout(parameterCompositeLayout);
-        GridData gd_parameterComposite = new GridData(SWT.FILL, SWT.FILL, true, true);
-        gd_parameterComposite.widthHint = 561;
+        GridData gd_parameterComposite = new GridData(SWT.NONE, SWT.TOP, true, false);
+        gd_parameterComposite.widthHint = 832;
         parameterComposite.setLayoutData(gd_parameterComposite);
 
         createParameterGroup(parameterComposite);
+        new Label(parameterComposite, SWT.NONE);
+        new Label(parameterComposite, SWT.NONE);
         new Label(parameterComposite, SWT.NONE);
 
     }
     
     private void createParameterGroup(Composite parent) {
+    	
     	parameterGroup = new Group(parent, SWT.NONE);
+    	GridData gd_parameterGroup = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+    	parameterGroup.setLayoutData(gd_parameterGroup);
         parameterGroup.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-        parameterGroup.setLayout(new GridLayout(3, false));
-        GridData gd_parameterGroup = new GridData(SWT.FILL, SWT.FILL, false, true);
+        parameterGroup.setLayout(new GridLayout(2, false));
+        GridData gd_parameterGroup = new GridData(SWT.FILL, SWT.FILL, true, true);
         
         label_a = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
         label_a.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
         label_a.setText("Schlüssel");
+        new Label(parameterGroup, SWT.NONE);
 
-        text_a = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_a = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
         text_a.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
         text_a.setText("Beschreibung");
         
         label_b = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
         label_b.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
         label_b.setText("Diffie Hellman Key Pair");
+        new Label(parameterGroup, SWT.NONE);
 
-        text_b = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_b = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
         text_b.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
         text_b.setText("Besteht aus einem öffentlichen und privaten Teil.");
         
         label_c = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
         label_c.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
         label_c.setText("Shared Secret Key");
+        new Label(parameterGroup, SWT.NONE);
 
-        text_c = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_c = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
         text_c.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
         text_c.setText("Geheimer Schlüssel der von den Kommunikationspartnern mittels einem vereinbarten Schlüsselaustausch erstellt wird. Dieser Schlüssel dient als erster Schlüssel");
         
         label_d = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
         label_d.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
         label_d.setText("Root Chain Key");
+        new Label(parameterGroup, SWT.NONE);
 
-        text_d = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_d = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
         text_d.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
         text_d.setText("");
         
-        label_e = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
+        label_e = new Text(parameterGroup, SWT.READ_ONLY);
         label_e.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
         label_e.setText("Sending Chain Key");
+        new Label(parameterGroup, SWT.NONE);
 
-        text_e = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_e = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
         text_e.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
         text_e.setText("");
         
         label_f = new Text(parameterGroup, SWT.READ_ONLY | SWT.CURSOR_ARROW);
         label_f.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
         label_f.setText("Receiving Chain Key");
+        new Label(parameterGroup, SWT.NONE);
 
-        text_f = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY);
+        text_f = new Text(parameterGroup, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
         text_f.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
         text_f.setText("");
     }

fcb84025a834375ebd0e7b0851b5c88715f42629
S2010239001 (Aigner Lorenz) <s2010239001@students.fh-hagenberg.at>
2021-05-12 07:23:34 +0000

    Testing overview merge

--- org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionView.java
+++ org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionView.java
@@ -9,44 +9,73 @@
 //-----END DISCLAIMER-----
 package org.jcryptool.visual.signalencryption.ui;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.custom.TableEditor;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+import org.jcryptool.core.logging.utils.LogUtil;
+import org.jcryptool.core.util.colors.ColorService;
+import org.jcryptool.core.util.fonts.FontService;
 import org.jcryptool.core.util.ui.TitleAndDescriptionComposite;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.jcryptool.core.util.ui.auto.SmoothScroller;
+
 
 
 /**
- * @author Lorenz Aigner
+ * 
  * @author Dan-Kerko Ciudin
- * @author Dominik Füreder
  * 
  */
 
 public class SignalEncryptionView extends ViewPart {
 	
-	
+	// Constants
+	public static final String ID = "org.jcryptool.visual.huffmanCoding.ui.SignalEncryptionView"; //$NON-NLS-1$
+//	public final static int OVERVIEW = 0;
+//	public final static int DOUBLERATCHET = 1;
+		
 	// Main UI components
 	private Composite parent;
-	private Composite overViewComposite;
+
 	private TabFolder tabFolder;
-	private TabItem tbtmAllgemein;
+	private TabItem tbtmOverview;
 	private TabItem tbtmDoubleRatchet;
 	private SignalEncryptionView mainView;
 	private SignalEncryptionViewDoubleRatchet doubleRatchetView;
 	
-	private TitleAndDescriptionComposite titleAndDescription;
+	// Tab-Handling
+    private int previousTab = 0;
+	private SignalEncryptionViewOverview overViewTab;
 	
 	
 	public SignalEncryptionView() {
@@ -64,31 +93,60 @@ public class SignalEncryptionView extends ViewPart {
 		scrolledComposite.setExpandHorizontal(true);
 		scrolledComposite.setExpandVertical(true);
 		
-		
+		// Tab folder for the tabs
 		tabFolder = new TabFolder(scrolledComposite, SWT.NONE);
 		
-		tbtmAllgemein = new TabItem(tabFolder, SWT.NONE);
-		tbtmAllgemein.setText("Allgemein");
+		// first tab for the key overview
+		tbtmOverview = new TabItem(tabFolder, SWT.NONE);
+		tbtmOverview.setText("Allgemein");
+
 		
+		// second tab for the Double Ratchet algorithm
 		tbtmDoubleRatchet = new TabItem(tabFolder, SWT.NONE);
 		tbtmDoubleRatchet.setText("Double Ratchet");
 		
-		// Init first tab
-		overViewComposite = new Composite(tabFolder, SWT.NONE);
-		tbtmAllgemein.setControl(overViewComposite);
-		GridLayout gl_overViewComposite = new GridLayout(2,false);
-		gl_overViewComposite.marginHeight = 0;
-		gl_overViewComposite.marginWidth = 0;
-		overViewComposite.setLayout(gl_overViewComposite);
+		//tab selection listener
+		tabFolder.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int selection = tabFolder.getSelectionIndex();
+				
+				tabFolder.setSelection(selection);
+				setTab(selection);
+			}
+		});
 		
-		// Content of first tab
-		titleAndDescription = new TitleAndDescriptionComposite(overViewComposite);
-		titleAndDescription.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1));
-		titleAndDescription.setTitle("Allgemein");
-		titleAndDescription.setDescription("Allgemeine Informationen");
+		//init first tab
+		overViewTab = new SignalEncryptionViewOverview(tabFolder, SWT.NONE);	
+		tbtmOverview.setControl(overViewTab);
+		                     
+		
+        scrolledComposite.setContent(tabFolder);
+        scrolledComposite.setMinSize(tabFolder.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		
+		// This makes the ScrolledComposite scrolling, when the mouse 
+		// is on a Text with one or more of the following tags: SWT.READ_ONLY,
+		// SWT.V_SCROLL or SWT.H_SCROLL.
+		SmoothScroller.scrollSmooth(scrolledComposite);
 	}
 	
-	
+
+	public void setTab(int tab) { 
+        switch (tab) {
+         case 0:         
+           tbtmOverview.setControl(overViewTab);
+           previousTab = 0;
+           break; 
+         case 1:   
+//           tbtmDoubleRatchet.setControl(DoubleRatchetTab);
+           previousTab = 1;
+        default:
+            break;
+        }
+
+        tabFolder.setSelection(tab);
+        FontService.getHeaderFont();
+    }
 	
 	@Override
 	public void setFocus() {
--- org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionViewDoubleRatchet.java
+++ org.jcryptool.visual.signalencryption/src/org/jcryptool/visual/signalencryption/ui/SignalEncryptionViewDoubleRatchet.java
@@ -1,5 +1,76 @@
 package org.jcryptool.visual.signalencryption.ui;
 
-public class SignalEncryptionViewDoubleRatchet {
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.jcryptool.core.util.ui.TitleAndDescriptionComposite;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.custom.CBanner;
 
+/**
+ * 
+ * @author Dan-Kerko Ciudin
+ * 
+ * 
+ */
+
+public class SignalEncryptionViewDoubleRatchet extends Composite {
+
+    private TitleAndDescriptionComposite titleAndDescription;
+
+    private GridData gd_aliceComposite;
+    private GridData gd_bobComposite;
+
+    SignalEncryptionViewDoubleRatchet(Composite parent, int style) {
+        super(parent, style);
+        GridLayout gridLayout = new GridLayout(2, false);
+        gridLayout.horizontalSpacing = 3;
+        gridLayout.verticalSpacing = 0;
+        setLayout(gridLayout);
+
+        Button btnAlice = new Button(this, SWT.NONE);
+        GridData gd_btnAlice = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+        gd_btnAlice.widthHint = 150;
+        btnAlice.setLayoutData(gd_btnAlice);
+
+        btnAlice.setText("Alice");
+
+        Button btnBob = new Button(this, SWT.RIGHT);
+        btnBob.setAlignment(SWT.CENTER);
+        GridData gd_btnBob = new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1);
+        gd_btnBob.widthHint = 150;
+        btnBob.setLayoutData(gd_btnBob);
+        btnBob.setText("Bob");
+        
+        Composite composite = new Composite(this, SWT.NONE);
+        composite.setLayout(new StackLayout());
+        composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 14, 1));
+        
+        Composite composite_1 = new Composite(composite, SWT.NONE);
+        
+        Composite composite_2 = new Composite(composite, SWT.NONE);
+        // setLayout(new StackLayout());
+
+        createAliceComposite();
+        createBobComposite();
+
+    }
+
+    private void createBobComposite() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void createAliceComposite() {
+        // TODO Auto-generated method stub
+
+    }
 }
